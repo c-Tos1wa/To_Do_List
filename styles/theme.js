75ae8dd1830085ec9766a0b8e19d781274826063
Cristina <cristoshiwassaki@gmail.com>
2021-09-22 15:30:40 -0300

    adição de fonte padrão

--- styles/theme.js
+++ styles/theme.js
@@ -1,6 +1,11 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const colors = {
+  gray:{
+    50: '#F0FFFF',
+    100: '#C4C4C4',
+    200: '#000000'
+  },
   brand: {
     900:'#AF2D2D',
     800:'#41AD4C',
@@ -10,6 +15,13 @@ const colors = {
   },
 }
 
-const theme = extendTheme({ colors });
+const fonts = {
+  heading: "Roboto Mono",
+  body: "Roboto Mono",
+  text: "Roboto Mono",
+  sansSerif: 'Ubuntu'
+}
+
+const theme = extendTheme({ colors, fonts });
 
 export default theme
