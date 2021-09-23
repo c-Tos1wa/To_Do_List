9fbd9a38dff07a3d92547ce77dbf25b237156a51
Cristina <cristoshiwassaki@gmail.com>
2021-09-23 12:42:41 -0300

    Layout da página de cadastro

--- pages/signUp.js
+++ pages/signUp.js
@@ -1,51 +1,83 @@
-import { Text, Flex, Input,  VStack, FormControl, FormLabel, Button, Link } from '@chakra-ui/react'
-
+import { Text, Box, Input,  VStack, FormControl, FormLabel, Container, Button, Link } from '@chakra-ui/react'
 
 export default function SignUp (){
  return (
    <>
-     <VStack alignItems='center' justifyContent='center'>
-        <Text>
+     <VStack 
+        bgColor='brand.500'
+        alignItems='center' 
+        justifyContent='center'
+        p='12'
+      >
+        <Text 
+          mb='5'
+          fontSize='xl'
+          fontWeight='bold'
+          textShadow='1px 1px #ffffff'
+        >
           Crie sua conta
         </Text>
-        <VStack >
+        <VStack 
+          bgColor='gray.50' 
+          borderWidth='1px' borderRadius='5' p='5'>
           <FormControl id='name'>
-            <FormLabel
-              justifyContent='center'
-            >Nome</FormLabel>
+            <FormLabel>
+              Nome
+            </FormLabel>
             <Input 
               type='text'
-              placeholder='Digite seu nome' 
+              placeholder='Digite seu nome'
+              size='lg' 
             />
           </FormControl>
           <FormControl id='username'>
-            <FormLabel>Username</FormLabel>
+            <FormLabel>
+              Username
+            </FormLabel>
             <Input
               type='text'
               placeholder='Digite um username'
+              size='lg'
             />
           </FormControl>
           <FormControl id='email'>
-            <FormLabel>Email</FormLabel>
+            <FormLabel>
+              Email
+            </FormLabel>
             <Input
               type='email'
               placeholder='Digite seu email'
+              size='lg'
             />
           </FormControl>
           <FormControl id='password'>
-            <FormLabel>Senha</FormLabel>
+            <FormLabel>
+              Senha
+            </FormLabel>
             <Input
               type='password'
               placeholder='Digite uma senha'
+              size='lg'
             />
           </FormControl>
           <Button
+            mt='5'
+            p='3'
             bgColor='brand.700'
             color='gray.50'
-          >CRIAR</Button>
+            borderRadius='5'
+            _hover={{
+              bgColor:'gray.50',
+              color:'brand.700'
+            }}
+          >
+              CRIAR 
+          </Button>
         </VStack>
-        <Text>
-          Se tiver uma conta, clique aqui
+        <Text
+          m='4'
+        >
+          Se tiver uma conta, clique aqui.
         </Text>
    </VStack>
 
