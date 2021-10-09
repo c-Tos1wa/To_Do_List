8269d8dcedd57cae8e5bec9795b0f9751a5329b5
bernaRocha <mrochabernardo@gmail.com>
2021-10-09 19:05:41 -0300

    corrigindo erros na página login

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/home":["static\u002Fchunks\u002Fpages\u002Fhome.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhome","\u002Flogin"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/home":["static\u002Fchunks\u002Fpages\u002Fhome.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhome"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/signUp":["static\u002Fchunks\u002Fpages\u002FsignUp.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002FsignUp"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> d46e0c2a1935d60a49e2a1d45eaa5b7003d15b40
->>>>>>> 559a71dd8b9fdae1e31610476ca6ca84978ab7ef
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/home":["static\u002Fchunks\u002Fpages\u002Fhome.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fhome","\u002Flogin"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
